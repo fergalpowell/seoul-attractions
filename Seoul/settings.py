977b247850e4cb957c61568aaa6c532aedab2672
fergalpowell <fergal.powell@gmail.com>
2018-05-28 02:07:10 +0100

    Moved static directory from root to attractions

--- Seoul/settings.py
+++ Seoul/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "attractions/static"),
     'path_to_static_directory/static/',
 ]
 
