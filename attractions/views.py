c45323c57d7fbd2acbe9c033c52b6dfebe70baf2
fergalpowell <fergal.powell@gmail.com>
2018-05-28 02:55:23 +0100

    Used static() to find csv file in attractions.view

--- attractions/views.py
+++ attractions/views.py
@@ -3,6 +3,7 @@ import csv
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import get_template
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 def load_map(request):
@@ -17,7 +18,7 @@ def load_attractions(request):
         'type': 'FeatureCollection',
         'features': []
     }
-    with open('../static/locations.csv', 'r') as csv_file:
+    with open(static('locations.csv'), 'r') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         for row in reader:
             geojson['features'].append({
